7c1b54354802be9846fb256a1c51f3866fd40a44
Ziwei Lan <ziweilan92@gmail.com>
2020-11-25 18:02:52 +1100

    add cors support

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -12,22 +12,22 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
-        private readonly DataContext context;
+        private readonly DataContext _context;
         public UsersController(DataContext context)
         {
-            this.context = context;
+            _context = context;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            return await context.Users.ToListAsync();
+            return await _context.Users.ToListAsync();
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            return await context.Users.FindAsync(id);
+            return await _context.Users.FindAsync(id);
         }
     }
 }
