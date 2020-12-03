510b2773aee1855a70ae641a8b8ca8a2f895c5ff
Ziwei Lan <ziweilan92@gmail.com>
2020-12-03 15:38:04 +1100

    add login logic

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,6 +37,26 @@ namespace Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+        {
+            var user = await _context.Users
+                .SingleOrDefaultAsync<AppUser>(x => x.UserName == loginDTO.UserName);
+
+            if(user == null) return Unauthorized("Invaid user name");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+            for (int i = 0; i < computedHash.Length; i++) 
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return user;
+        }
+
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username);
---
+++ API/DTOs/LoginDTO.cs
@@ -0,0 +1,9 @@
+namespace DTOs
+{
+    public class LoginDTO
+    {
+        public string UserName { get; set; }
+      
+        public string Password { get; set; }
+    }
+}
