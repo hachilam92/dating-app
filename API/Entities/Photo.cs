afdd35e40488e3a2f44eb8fb23a5569bcfe74a47
Ziwei Lan <ziweilan92@gmail.com>
2020-12-17 15:30:57 +1100

    update database with extended entities

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Entities
 {
+	[Table("Photos")]
 	public class Photo
 	{
 		public int Id { get; set; }
 		public int Url { get; set; }
 		public bool IsMain { get; set; }
 		public string PublicId { get; set; }
+		public AppUser AppUser { get; set; }
+		public int AppUserId { get; set; }
 	}
 }
