f18fa3f26642126ce441e3036cb14fc6626aae59
Ziwei Lan <ziweilan92@gmail.com>
2020-12-17 22:53:29 +1100

    bug fix

--- API/Startup.cs
+++ API/Startup.cs
@@ -5,9 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using Extensions;
 using Middleware;
 
