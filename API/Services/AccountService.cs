c084d81dcdf025a884a73a317193b4bece15900d
Ziwei Lan <ziweilan92@gmail.com>
2021-02-01 16:37:41 +1100

    fix typo

--- API/Services/AccountService.cs
+++ API/Services/AccountService.cs
@@ -12,57 +12,58 @@ using System.Threading.Tasks;
 
 namespace Services
 {
-  public class AccountService : IAccountService
-  {
-    private readonly UserManager<AppUser> _userManager;
-    private readonly SignInManager<AppUser> _signInManager;
-    private readonly IMapper _mapper;
-    public AccountService(
-        UserManager<AppUser> userManager,
-        SignInManager<AppUser> signInManager,
-        IMapper mapper
-    ) {
-        _userManager = userManager;
-        _signInManager = signInManager;
-        _mapper = mapper;
-    }
-    public async Task<AppUser> AddUser(RegisterDTO registerDTO)
+    public class AccountService : IAccountService
     {
-        if (await UserExists(registerDTO.UserName)) return null;
-
-            var user = _mapper.Map<AppUser>(registerDTO);
-
-        user.UserName = registerDTO.UserName;
-
-        var result = await  _userManager.CreateAsync(user, registerDTO.Password);
-        
-        if (!result.Succeeded) throw new BadRequestException(result.Errors.ToString());
-
-        var roleResult = await _userManager.AddToRoleAsync(user, "Member");
-
-        if (!roleResult.Succeeded) throw new BadRequestException(result.Errors.ToString());
-
-            return user;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IMapper _mapper;
+        public AccountService(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            IMapper mapper
+        ) {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
         }
+        public async Task<AppUser> AddUser(RegisterDTO registerDTO)
+        {
+            if (await UserExists(registerDTO.UserName)) return null;
 
-    public async Task<AppUser> VerifyUser(LoginDTO loginDTO)
-    {
-        var user = await _userManager.Users
-            .Include(p => p.Photos)
-            .SingleOrDefaultAsync<AppUser>(x => x.UserName == loginDTO.UserName);
+                var user = _mapper.Map<AppUser>(registerDTO);
 
-        if(user == null) throw new UnauthorizedException("Invalid user name or password");
+            user.UserName = registerDTO.UserName;
 
-        var result = await _signInManager
-            .CheckPasswordSignInAsync(user, loginDTO.Password, false);
+            var result = await  _userManager.CreateAsync(user, registerDTO.Password);
+            
+            if (!result.Succeeded) throw new BadRequestException(result.Errors.ToString());
 
-        if(!result.Succeeded) throw new UnauthorizedException("Invalid user name or password");
+            var roleResult = await _userManager.AddToRoleAsync(user, "Member");
 
-            return user;
+            if (!roleResult.Succeeded) throw new BadRequestException(result.Errors.ToString());
+
+                return user;
+            }
+
+        public async Task<AppUser> VerifyUser(LoginDTO loginDTO)
+        {
+            var user = await _userManager.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync<AppUser>(x => x.UserName == loginDTO.UserName);
+
+            if(user == null) throw new UnauthorizedException("Invalid user name or password");
+
+            var result = await _signInManager
+                .CheckPasswordSignInAsync(user, loginDTO.Password, false);
+
+            if(!result.Succeeded) throw new UnauthorizedException("Invalid user name or password");
+
+                return user;
+            }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _userManager.Users.AnyAsync(x => x.UserName == username);
         }
-
-    private async Task<bool> UserExists(string username)
-    {
-        return await _userManager.Users.AnyAsync(x => x.UserName == username);
     }
 }
